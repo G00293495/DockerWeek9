22cd187d241542f85ff687f3f1c46d45965727a7
Paddy downey <G00392495@Atu.ie>
2023-11-22 15:37:35 +0000

    Created docker app and ran hello world website

---
+++ src/test/java/ie/atu/week9/DockerAppApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week9;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DockerAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
